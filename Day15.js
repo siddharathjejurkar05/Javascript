b7f7e080117f0336ed10b6a8bfbdf2d757007979
Siddharath Sachin Jejurkar <siddharathjejurkar05@rediffmail.com>
2026-07-01 12:10:47 +0530

    Add files via upload

---
+++ Day15.js
@@ -0,0 +1,52 @@
+// Event Loop In JavaScript -->
+
+// JS --> Is a single threaded synchronous language 
+ 
+// console.log("first");
+// let sum = 0;
+// for(let i=0;i<3000000000;i++)
+//     sum+=i;
+
+// console.log(sum);
+// console.log("Last");
+
+
+// console.log("Hello Ji");
+
+// setTimeout(()=>{
+//     console.log("Time is Executed");
+// },5000);
+
+// setTimeout(()=>{
+//     console.log("Time is Executed2");
+// },6000);
+
+// console.log("I am End");
+
+// console.log("hello ji");
+
+// const button1 = document.getElementById('button1');
+// button1.addEventListener('click',()=>{
+//     console.log('Button 1 is clicked....');
+// });
+
+// const button2 = document.getElementById('button2');
+// button2.addEventListener('click',()=>{
+//     console.log('Button 2 is clicked....');
+// });
+
+// const button3 = document.getElementById('button3');
+// button3.addEventListener('click',()=>{
+//     console.log('Button 3 is clicked....');
+// });
+
+// console.log("I am the End../"); 
+
+
+// console.log("Start the Operation");
+
+// fetch("https://api.github.co m/users").then(()=>{
+//     console.log("Git Hub user info");
+// })
+
+// console.log("end of operation");
